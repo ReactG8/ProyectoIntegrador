7950942be5fc89e53e303884a4a0946ea7c4407e
Henry Alonzo <henryalonzo.dev@gmail.com>
2023-11-22 01:53:11 -0500

    se agrega configuracion de FIREBASE

---
+++ firebaseConfig/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG0yQ0rqE0TrYZCn2riQfUOJgHRRZPAq0",
+  authDomain: "ferreteria-mibarrio.firebaseapp.com",
+  projectId: "ferreteria-mibarrio",
+  storageBucket: "ferreteria-mibarrio.appspot.com",
+  messagingSenderId: "291884527308",
+  appId: "1:291884527308:web:053453f2c1251f28ea1c69",
+  measurementId: "G-GX4X2FLVLR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);

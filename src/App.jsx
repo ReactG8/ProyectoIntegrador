ed118503426b3de2f1eb9276aca9155d12055a85
Henry Alonzo <henryalonzo.dev@gmail.com>
2023-11-30 18:59:59 -0500

    agrego route para el login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Create } from "./components/Create";
 import { Edit } from "./components/Edit";
 import { Nav } from "./components/Nav";
 import { Footer } from "./components/Footer";
+import  Login from "./components/Login"; 
 
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
           <Route path="/" element={<Show />} />
           <Route path="/create" element={<Create />} />
           <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>

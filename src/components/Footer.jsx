6f845b28382f8a7d3ee521122d280da2f3ced13a
ceciliagebhard <cirakarma@gmail.com>
2023-12-11 13:39:06 -0300

    Se editan atributos del Footer y la NavBar para que sean responsive en distintos dispositivos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,31 @@
 export const Footer = () => {
   return (
-    <footer className="bg-dark text-white fixed-bottom">
-      <ul className="nav justify-content-center border-bottom mb-2">
-        <li className="nav-item">
-          <a href="#!" className="nav-link px-2 text-white">
-            <i className="fa-brands fa-instagram fa-xl"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#!" className="nav-link px-2 text-white">
-            <i className="fa-brands fa-facebook fa-xl"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#!" className="nav-link px-2 text-white">
-            <i className="fa-brands fa-linkedin fa-xl"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#!" className="nav-link px-2 text-white">
-            <i className="fa-brands fa-whatsapp fa-xl"></i>
-          </a>
-        </li>
-      </ul>
-      <p className="text-center text-white">© Grupo 8 - Codo a Codo 2023</p>
+    <footer className="bg-dark text-white fixed-bottom d-flex flex-wrap justify-content-center">
+      <div>
+        <ul className="nav navbar-expand-lg justify-content-center border-bottom mb-2">
+          <li className="nav-item">
+            <a href="#!" className="nav-link px-2 text-white">
+              <i className="fa-brands fa-instagram fa-xl"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#!" className="nav-link px-2 text-white">
+              <i className="fa-brands fa-facebook fa-xl"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#!" className="nav-link px-2 text-white">
+              <i className="fa-brands fa-linkedin fa-xl"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#!" className="nav-link px-2 text-white">
+              <i className="fa-brands fa-whatsapp fa-xl"></i>
+            </a>
+          </li>
+        </ul>
+        <p className="text-center text-white">© Grupo 8 - Codo a Codo 2023</p>
+      </div>
     </footer>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,19 +5,20 @@ import logo from "../assets/Logo6Chico.png";
 export const Nav = () => {
 
   const location = useLocation();
-  const currentView = location.pathname; 
+  const currentView = location.pathname;
 
   const allowedRoutesLoginContact = ["/"];
   const shouldShowLinkLoginContact = allowedRoutesLoginContact.includes(currentView);
 
   const allowedSignOutCreateService = ["/admin"];
   const shouldShowLinkSignOutCreateService = allowedSignOutCreateService.includes(currentView);
-  
+
   return (
 
     <nav
       className="navbar navbar-expand-lg navbar-dark bg-dark border-body fixed-top shadow d-flex"
     >
+      <div>
         <a className="navbar-brand p-2" href="/">
           <img
             src={logo}
@@ -31,46 +32,47 @@ export const Nav = () => {
         <ul className="navbar-nav m-auto">
 
           {shouldShowLinkLoginContact && (
-          <li className="nav-link">
-            <Link className="nav-link text-warning" to={"/login"}>
-              <i className="fa-solid fa-user"></i> Ingresar  
-            </Link>
-          </li>
+            <li className="nav-link">
+              <Link className="nav-link text-warning" to={"/login"}>
+                <i className="fa-solid fa-user"></i> Ingresar
+              </Link>
+            </li>
           )}
 
           {shouldShowLinkLoginContact && (
-          <li className="nav-link">
-            <Link className="nav-link text-warning" to={"/contact"}>
-              <i className="fa fa-comments"></i> Contacto
-            </Link>
-          </li>
+            <li className="nav-link">
+              <Link className="nav-link text-warning" to={"/contact"}>
+                <i className="fa fa-comments"></i> Contacto
+              </Link>
+            </li>
           )}
 
           {shouldShowLinkSignOutCreateService && (
-          <li className="nav-link">
-            <Link className="nav-link text-warning" to={"/signout"}>
-            <i className="fa-solid fa-down-long"></i> Salir
-            </Link>
-          </li>
+            <li className="nav-link">
+              <Link className="nav-link text-warning" to={"/signout"}>
+                <i className="fa-solid fa-down-long"></i> Salir
+              </Link>
+            </li>
           )}
-          
+
           {shouldShowLinkSignOutCreateService && (
-          <li className="nav-link">
-            <Link className="nav-link text-warning fa-beat-fade" to={"/create"}>
-              <i className="fa-solid fa-plus"></i> Crear Producto
-            </Link>
-          </li>
+            <li className="nav-link">
+              <Link className="nav-link text-warning fa-beat-fade" to={"/create"}>
+                <i className="fa-solid fa-plus"></i> Crear Producto
+              </Link>
+            </li>
           )}
-          
+
           {shouldShowLinkSignOutCreateService && (
-          <li className="nav-link">
-            <Link className="nav-link text-warning" to={"/comentarios"}>
-              Servicio al Cliente
-            </Link>
-          </li>
+            <li className="nav-link">
+              <Link className="nav-link text-warning" to={"/comentarios"}>
+                Servicio al Cliente
+              </Link>
+            </li>
           )}
 
         </ul>
+      </div>
     </nav>
   )
 }

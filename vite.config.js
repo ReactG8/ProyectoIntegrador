0b19f20c51d95f9b22023369e700427424f681ea
adaarg <ada.arg@gmail.com>
2023-12-12 16:23:41 -0300

    Haciendo el deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ProyectoIntegrador/",
-  build: { chunkSizeWarningLimit: 1600, }
+  build: { chunkSizeWarningLimit: 1600, },
+  base: "/ProyectoIntegrador/"
 })
